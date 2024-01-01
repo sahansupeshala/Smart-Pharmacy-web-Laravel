0e98b38cd6c9244c9ca8113cc0474bccd0d5800c
sahansupeshala <142963520+sahansupeshala@users.noreply.github.com>
2024-01-01 15:39:00 +0530

    pharmcy commit

---
+++ app/Models/PreparedQuotation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PreparedQuotation extends Model
+{
+    use HasFactory;
+    
+}
